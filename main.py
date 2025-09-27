3fbc55c6b1561c81dfac44d325b7292ce3fc460b
HasnatArnab <hasnatornob00@gmail.com>
2025-09-27 20:00:51 +0600

    made some changes to main.py & stats.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from stats import get_num_words
+import sys
+from stats import get_num_words,get_num_char,sorted_list,generate_report
 
 def main():
-    contents=get_book_text('books/frankenstein.txt')
-    print(get_num_words(contents))
-    
+    if len(sys.argv)!=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        filepath=sys.argv[1]
+        contents=get_book_text(filepath)
+        word_count=get_num_words(contents)
+        char_list=get_num_char(contents)
+        list=sorted_list(char_list)
+        generate_report(filepath,word_count,list)
+        
 def get_book_text(path_file):
     '''this function takes the filepath as argument and returns the content of that file'''
     with open(path_file) as f:
         file_content=f.read()
         return file_content
 
-
-
-    
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,4 +2,35 @@ def get_num_words(content):
     text=content.split()
     num_words = len(text)
     # print(text)
-    return f"Found {num_words} total words"
+    return num_words
+
+def get_num_char(content):
+    char_list={}
+    for i in content:
+        i=i.lower()
+        if not(i in char_list):
+            char_list[i]=1
+        else:
+            char_list[i]+=1
+    return char_list
+
+def sorted_list(char_list):
+    updated_list=[]
+    for i in char_list:
+        if i.isalpha():
+            updated_list.append({"char":i, "num":char_list[i]})
+    updated_list.sort(reverse=True,key=sort_on)
+    return updated_list
+
+def sort_on(items):
+    return items["num"]
+
+def generate_report(filepath,word_count,list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print(f"--------- Character Count -------")
+    for i in list:
+        print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
